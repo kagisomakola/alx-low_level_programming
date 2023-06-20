3a9baec2e1410fb0db367ed99c33906f2748f4aa
kagiso <kagisomakola01@gmail.com>
2023-06-20 15:25:51 -0700

    1-alphabet

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,8 +1,9 @@
 #include <unistd.h>
  
- 
+
+
 /**
- * _putchar - Writes the character c to stdout
+ * _putchar - Writes the character C to stdout
  * @C: The character to print
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
