6d29cd366318bbc4e1ca6546dd96118f3fba5872
kagiso <kagisomakola01@gmail.com>
2023-06-22 06:33:47 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char C;
+
+	C = 'a';
+	while
+		(C <= 'z') {
+			putchar(C);
+			C++;
+
+		}
+	putchar('\n');
+	return (0);
+}
