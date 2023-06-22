d06d3eba128e990a89811dd3d6820f1b5a736746
kagiso <kagisomakola01@gmail.com>
2023-06-22 06:44:38 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char C;
+
+	char d;
+
+	C = 'a';
+	d = 'A';
+	while
+		(C <= 'z') {
+			putchar(C);
+			C++;
+		}
+	while
+		(d <= 'z') {
+			putchar (d);
+			d++;
+
+		}
+	putchar('\n');
+	return (0);
+}
+
