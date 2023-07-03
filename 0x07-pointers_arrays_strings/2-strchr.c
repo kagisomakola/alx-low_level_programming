090eac9568506ee9265d43fbf6e0c0f0c80f14f6
kagiso <kagisomakola01@gmail.com>
2023-07-03 15:25:06 -0700

    2-strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: Always 0 (success)
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	for (; s[1] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (0);
+}
