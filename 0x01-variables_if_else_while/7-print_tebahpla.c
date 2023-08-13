06990ca8a6c9ef5ba86419e12a702e67f1004566
kagisomakola <kagisomakola01@gmail.com>
2023-08-13 15:03:44 -0700

    print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always zero (success)
+ */
+
+int main(void)
+{
+
+	char C;
+
+	C = 'z';
+	while
+
+		(C >= 'a') {
+			putchar(C);
+			C--;
+
+		}
+putchar('\n');
+return (0);
+}
