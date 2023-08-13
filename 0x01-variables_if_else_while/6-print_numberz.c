7364e44264feff6773ebd1bdfc3c72da67679309
kagisomakola <kagisomakola01@gmail.com>
2023-08-13 14:54:23 -0700

    print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always zero (success)
+ */
+
+int main(void)
+{
+	int C;
+
+	C = 0;
+	while
+		(C < 10) {
+			putchar('C');
+			C++;
+
+		}
+	putchar('\n');
+	return (0);
+}
