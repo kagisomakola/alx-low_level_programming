3d5ae63d7897126c0a8a13c7d1f4bbbbe2db3bca
kagisomakola <kagisomakola01@gmail.com>
2023-08-13 15:33:54 -0700

    print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,19 +4,19 @@
  * Return: Always zero (success)
  */
 int main(void)
-{
+	{
 	int a;
 
-	for (a = 0; a < 10; a++) 
+	for (a = 0; a < 10; a++)
 	{
 		putchar(a + '0');
 		if (a < 9)
-		{	
+		{
 			putchar(',');
-			putchar(',');
+			putchar(' ');
 
 		}
 	}
 	putchar('\n');
 	return (0);
-}
+	}
