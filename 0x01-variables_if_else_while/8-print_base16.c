82d06956f98f140f6a559d51f722aec83a33b57e
kagisomakola <kagisomakola01@gmail.com>
2023-08-13 15:12:41 -0700

    print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always zero (success)
+ */
+int main(void)
+{
+	char C;
+
+	int d;
+
+	C = 'a';
+	while
+		(d < 10) {
+			putchar(d + '0');
+			d++;
+		}
+	while
+		(C <= 'f') {
+			putchar(C);
+			C++;
+		}
+	putchar('\n');
+	return (0);
+}
