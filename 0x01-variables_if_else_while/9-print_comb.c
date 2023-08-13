be0d149a427208ad70252c5b816f1c3f81991a9e
kagisomakola <kagisomakola01@gmail.com>
2023-08-13 15:20:45 -0700

    print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always zero (success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 100; a++) {
+		putchar(a + '0');
+		if (a < 99) {
+			putchar(',');
+			putchar(',');
+
+		}
+	}
+	putchar('\n');
+	return (0);
+}
