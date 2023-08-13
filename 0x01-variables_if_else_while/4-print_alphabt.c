a606d74a10a2c5586293908341a2c70568e524f6
kagisomakola <kagisomakola01@gmail.com>
2023-08-13 14:32:01 -0700

    print_alphabet.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always zero (success)
+ */
+
+int main(void)
+{
+	char C;
+
+	C = 'a';
+	while
+		(C <= 'z') {
+			if (( C != 'q' && C != 'e') && C <= 'z')
+				putchar(C);
+			C++;
+
+		}
+	putchar('\n');
+	return (0);
+}
+
+
