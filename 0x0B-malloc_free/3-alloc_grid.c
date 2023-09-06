6e1f98a99dbab7fe7d57ea019a087b18deb7e77c
kagisomakola <kagisomakola01@gmail.com>
2023-09-06 03:55:13 -0700

    mollac

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,24 +11,24 @@ int **alloc_grid(int width, int height)
 {
 	int **mee;
 	int x, y;
-	
+
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	
+
 	mee = malloc(sizeof(int *) * height);
-		
+
 		if (mee == NULL)
 			return (NULL);
-	
+
 	for (x = 0; x < height; x++)
 	{
 		mee[x] = malloc(sizeof(int) * width);
-		
+
 		if (mee[x] == NULL)
 		{
 			for (; x >= 0; x--)
 				free(mee[x]);
-			
+
 			free(mee);
 			return (NULL);
 		}
@@ -38,6 +38,6 @@ int **alloc_grid(int width, int height)
 		for (y = 0; y < width; y++)
 			mee[x][y] = 0;
 	}
-	
+
 	return (mee);
 }
