2bbd46f018b82e77d29c579e7a44278e843d4d7a
kagisomakola <kagisomakola01@gmail.com>
2023-09-11 02:15:02 -0700

    0X0E

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -4,7 +4,7 @@
 /**
  * init_dog - initializes a variable of type struct dog
  * @d: pointer to struct dog to initialize
- * @name: name to initialize 
+ * @name: name to initialize
  * @age: age to initialize
  * @owner: owner to initialize
  */
