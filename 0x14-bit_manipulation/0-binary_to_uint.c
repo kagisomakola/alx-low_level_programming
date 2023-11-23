bfa2816d7fa24794f3967385f5f4c93bc43e3c8f
kagiso <kagisomakola01@gmail.com>
2023-11-23 13:54:50 -0800

    0x14

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,7 +8,7 @@
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
-	unsigned int dec_val = 0;
+	unsigned int des_val = 0;
 
 	if (!b)
 		return (0);
@@ -17,8 +17,8 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		dec_val = 2 * dec_val + (b[i] - '0');
+		des_val = 2 * des_val + (b[i] - '0');
 	}
 
-	return (dec_val);
+	return (des_val);
 }
